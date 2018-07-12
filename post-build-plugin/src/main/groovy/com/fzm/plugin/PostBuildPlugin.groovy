a2b91127c687d6a8227b734e42204eb8dbd625c5
bsding <dbsdingbaosheng@163.com>
2018-07-12 10:57:49 +0800

    1.hook assembleDebug task

--- post-build-plugin/src/main/groovy/com/fzm/plugin/PostBuildPlugin.groovy
+++ post-build-plugin/src/main/groovy/com/fzm/plugin/PostBuildPlugin.groovy
@@ -2,17 +2,15 @@ package com.fzm.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task
 
 class PostBuildPlugin implements Plugin<Project> {
 
     void apply(Project project) {
 
-//        project.extensions.create('postBuildConfig', PostBuildExtension)
 
-        Task uploadTask = project.task('upload') {
+        project.afterEvaluate {
 
-            doLast {
+            project.assembleRelease.doLast {
 
                 def jenkinsJobName = System.getenv('JOB_NAME') ?: "local_job"
                 def jenkinsBuild = System.getenv('BUILD_NUMBER') ?: "0"
@@ -21,7 +19,7 @@ class PostBuildPlugin implements Plugin<Project> {
                 def shouldUpload = project.getProperties().get('shouldUpload', false)
 
                 //判断是否需要执行拷贝文件，如果不是jenkins上build直接返回
-                if (jenkinsJobName != 'local_job') {
+                if (jenkinsJobName == 'local_job') {
                     //do nothing
                     return
                 }
@@ -71,7 +69,7 @@ class PostBuildPlugin implements Plugin<Project> {
 //                    println('current path:'+pythonScriptPath)
 
                     String[] cmd = new String[3];
-                    cmd[0] = "python"; // check version of installed python: python -V
+                    cmd[0] = "python" // check version of installed python: python -V
                     cmd[1] = pythonScriptPath
                     cmd[2] = vName
 
@@ -97,9 +95,45 @@ class PostBuildPlugin implements Plugin<Project> {
                 }
 
             }
-        }
 
-        uploadTask.dependsOn('assembleRelease')
+
+
+            project.assembleDebug.doLast{
+
+                def jenkinsJobName = System.getenv('JOB_NAME') ?: "local_job"
+                def jenkinsBuild = System.getenv('BUILD_NUMBER') ?: "0"
+
+                def vName = project.getProperties().get('vName', project.rootProject.ext.configProps.DEFAULT_VERSION_NAME)
+                def shouldUpload = project.getProperties().get('shouldUpload', false)
+
+                //判断是否需要执行拷贝文件，如果不是jenkins上build直接返回
+                if (jenkinsJobName == 'local_job') {
+                    //do nothing
+                    return
+                }
+
+
+                def fileBackPath = project.rootProject.ext.configProps.JENKINS_FILE_BACK_DIR + "/" + jenkinsJobName + "/" + jenkinsBuild
+
+                println("==========jenkinsJobName" + jenkinsJobName + "===========")
+                println("==========jenkinsBuild:" + jenkinsBuild + "===========")
+                println("==========vName:" + vName + "===========")
+                println("==========shouldUpload:" + shouldUpload + "===========")
+                println("==========fileBackPath:" + fileBackPath + "===========")
+
+                println("==========begain copy apk file===========")
+
+                project.copy {
+                    from('build/outputs/apk/debug')
+                    into(fileBackPath + '/apks')
+                    include('*.apk', '*.txt')
+                    exclude('**/*-unaligined.apk')
+                }
+//
+                println("==========end copy apk file===========")
+
+            }
+        }
 
     }
 }
